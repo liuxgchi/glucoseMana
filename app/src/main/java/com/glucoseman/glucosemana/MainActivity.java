90243737743b6bb9b795c0649393b52954e67191
liuxgchi <liuxgchi@qq.com>
2015-05-14 18:08:58 +0800

    branch test

--- app/src/main/java/com/glucoseman/glucosemana/MainActivity.java
+++ app/src/main/java/com/glucoseman/glucosemana/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends BaseActivity {
     @Override
     public void initDatas() {
         LogUtil.outLogDetail("test git status again");
+        LogUtil.outLogDetail("test git branch again");
     }
 
     @Override
